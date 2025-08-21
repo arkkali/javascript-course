56e253c5db556bd1760e2313860a7dd5cfd59432
Len Alegre <lenalegre4@gmail.com>
2025-08-21 14:53:20 +0800

    Exercise 3: Data Type Detective

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,10 +1,10 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
 
-console.log("4. Log it again");
-console.log("5. Try the same with const - what happens?");
-console.log("   It shows uncaught typeError");
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
 
-const score = "100"
-score = 150
-console.log(score);
+// What do you think each one will return?
+// number, string, boolean, undefined
