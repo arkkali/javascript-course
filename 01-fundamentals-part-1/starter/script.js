f2f0f369433b120707ee1bd9974c286deac04267
Len Alegre <lenalegre4@gmail.com>
2025-08-21 21:23:47 +0800

    04_Exercise 2: Fix the Bug

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,13 +1,6 @@
-// Exercise 1: Conversion Detective
+// Exercise 2: Fix the Bug
 
-console.log("5" + 2); 
-console.log("5" - 2); 
-console.log("5" * 2); 
-console.log("5" / 2); 
-
-// Convert explicitly
-const userAge = "25"; 
-const userScore = 95;
-
-console.log(Number(userAge)); 
-console.log(String(userScore)); 
+const num1 = Number(prompt("First number:")); // convert to number
+const num2 = Number(prompt("Second number:")); // convert to number
+const sum = num1 + num2;
+console.log(`Sum: ${sum}`);
