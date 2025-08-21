8618269c695abaa24dcca50b01d57980698182c8
Len Alegre <lenalegre4@gmail.com>
2025-08-21 16:49:08 +0800

    03_Exercise 1: Personal Introduction

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,46 +1,13 @@
-// JavaScript Fundamentals - Hour 2
+//Exercise 1: Personal Introduction
 
-// Exercise: Score Calculator
-let score = 100;
-console.log("Score:", score)
-score += 50
-console.log("After score += 50:",score)
-score *= 2
-console.log("After score *= 2:",score)
-score -= 25
-console.log("After score -= 25:",score)
-score ++;
-console.log("After score ++:",score)
-score ++;
-console.log("After score ++:",score)
+// Create variables for:
+const myName = "Len"; // Your name
+const myAge = 20; // Your age
+const myJob = "IT Student"; // Your job/student status
+const currentYear = 2025;
 
-///Exercise: Age Verification
-const myAge = 20;
-
-const compare = myAge >= 18;
-console.log("Is 18 less than my age?:", compare)
-
-const check = 100 >= 90;
-console.log("Is 100 greater than 90?:", check)
-
-console.log("Complex comparison:");
-console.log(compare > check); 
-
-// Coding Challenge #1 - BMI Calculator
-// Test Data 1
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-
-// 1. Calculate BMIs
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-
-// 2. Create markHigherBMI variable
-const markHigherBMI = BMIMark > BMIJohn;
-
-// 3. Log results to console
-console.log("Mark's BMI:", BMIMark);
-console.log("John's BMI:", BMIJohn);
-console.log("Does Mark have a higher BMI?", markHigherBMI);
+const me = "I'm " + myName + ", a " + myAge + " year old " + myJob + "!"
+console.log(me)
+// Use template literals to create:
+// "Hi, I'm [name], a [age] year old [job]!"
+// Log it to the console
