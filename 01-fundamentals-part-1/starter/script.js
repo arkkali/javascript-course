277aeb844077c57f5904a2f368dfc38f89d8195b
Len Alegre <lenalegre4@gmail.com>
2025-08-21 14:33:35 +0800

    Exercise 1: Personal Info Variables

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,24 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+console.log("1. Your first name (const)");
+const firstName = " Len -";
+console.log(firstName, typeof firstName);
+
+console.log("2. Your age (let - it changes every year!)");
+let age = 15
+age = 16
+age = 17
+age = 18
+age = 19
+age = 20
+console.log(age, typeof age);
+
+console.log("3. Your favorite number (const)");
+const faveNumber = "   4 -";
+console.log(faveNumber, typeof faveNumber);
+
+console.log("4. Whether you like JavaScript so far (boolean)");
+let iLikejavascript = true;
+console.log(iLikejavascript, typeof iLikejavascript);
+
