d20174e536d45ea90cfa786cd108b06200b9e6cb
Len Alegre <lenalegre4@gmail.com>
2025-08-21 14:43:28 +0800

    Exercise 2: Variable Reassignment (1&2)

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,24 +1,9 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
 
-console.log("1. Your first name (const)");
-const firstName = " Len -";
-console.log(firstName, typeof firstName);
+console.log("1. Create a let variable called 'score' with value 100");
+console.log("2. Log it to the console");
 
-console.log("2. Your age (let - it changes every year!)");
-let age = 15
-age = 16
-age = 17
-age = 18
-age = 19
-age = 20
-console.log(age, typeof age);
-
-console.log("3. Your favorite number (const)");
-const faveNumber = "   4 -";
-console.log(faveNumber, typeof faveNumber);
-
-console.log("4. Whether you like JavaScript so far (boolean)");
-let iLikejavascript = true;
-console.log(iLikejavascript, typeof iLikejavascript);
-
+let score = 100
+score = 150
+console.log(score);
