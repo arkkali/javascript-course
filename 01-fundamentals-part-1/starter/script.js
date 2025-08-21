854f02c378e7d386bd74eae42e4fbdb37766af89
Len Alegre <lenalegre4@gmail.com>
2025-08-21 15:02:38 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,10 +1,48 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// JavaScript Fundamentals - Hour 1
 
+//Exercise 1: Personal Info Variables
+// 1. Your first name (const)" 
+const firstName = " Len -";
+console.log(firstName, typeof firstName);
+
+// 2. Your age (let - it changes every year!)
+let age = 15
+age = 16
+age = 17
+age = 18
+age = 19
+age = 20
+console.log(age, typeof age);
+
+// 3. Your favorite number (const)
+const faveNumber = "   4 -";
+console.log(faveNumber, typeof faveNumber);
+
+// 4. Whether you like JavaScript so far (boolean)"
+let iLikejavascript = true;
+console.log(iLikejavascript, typeof iLikejavascript);
+
+
+//Exercise 2: Variable Reassignment
+// 1. Create a let variable called 'score' with value 100"
+// 2. Log it to the console
+// 3. Change it to 150
+
+let score = 100
+score = 150
+console.log(score);
+
+// 4. Log it again")
+    //const score = "100"
+    //score = 150
+    //console.log(score);
+// 5. Try the same with const - what happens?" = It shows uncaught typeError
+
+
+//Exercise 3: Data Type Detective
 console.log(typeof 42);
 console.log(typeof "Hello");
 console.log(typeof true);
 console.log(typeof undefined);
-
 // What do you think each one will return?
 // number, string, boolean, undefined
