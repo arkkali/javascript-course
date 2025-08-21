a32b7f65c6f1332700054945fbed2cb0985896e8
Len Alegre <lenalegre4@gmail.com>
2025-08-21 17:46:18 +0800

    Challenge #2: BMI Comparison

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,13 +1,22 @@
-// Exercise: Truth Detector
-const values = [0, 1, "", "hello", undefined, null, NaN, {}, []];
+// Coding Challenge #2: BMI Comparison
 
-for (let val of values) {
-  console.log("Value:", val);
-  console.log("Boolean:", Boolean(val));
-  if (val) {
-    console.log("Truthy!");
-  } else {
-    console.log("Falsy!");
-  }
-  console.log("-----------------");
+// Reuse your BMI calculation from Challenge #1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log("Mark's BMI:", BMIMark);
+console.log("John's BMI:", BMIJohn);
+
+// Compare BMIs with if/else and use template literals
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else if (BMIJohn > BMIMark) {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+} else {
+  console.log(`Wow! Mark and John have the same BMI (${BMIMark})`);
 }
+
