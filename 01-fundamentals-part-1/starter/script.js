aa31007a1613bcb7768deffba2ba4e5a68abf585
Len Alegre <lenalegre4@gmail.com>
2025-08-21 15:33:50 +0800

    02_Exercise: Score Calculato

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,48 +1,18 @@
-// JavaScript Fundamentals - Hour 1
+// Exercise: Score Calculator
+let score = 100;
+console.log("Score:", score)
 
-//Exercise 1: Personal Info Variables
-// 1. Your first name (const)" 
-const firstName = " Len -";
-console.log(firstName, typeof firstName);
+score += 50
+console.log("After score += 50:",score)
 
-// 2. Your age (let - it changes every year!)
-let age = 15
-age = 16
-age = 17
-age = 18
-age = 19
-age = 20
-console.log(age, typeof age);
+score *= 2
+console.log("After score *= 2:",score)
 
-// 3. Your favorite number (const)
-const faveNumber = "   4 -";
-console.log(faveNumber, typeof faveNumber);
+score -= 25
+console.log("After score -= 25:",score)
 
-// 4. Whether you like JavaScript so far (boolean)"
-let iLikejavascript = true;
-console.log(iLikejavascript, typeof iLikejavascript);
+score ++;
+console.log("After score ++:",score)
 
-
-//Exercise 2: Variable Reassignment
-// 1. Create a let variable called 'score' with value 100"
-// 2. Log it to the console
-// 3. Change it to 150
-
-let score = 100
-score = 150
-console.log(score);
-
-// 4. Log it again")
-    //const score = "100"
-    //score = 150
-    //console.log(score);
-// 5. Try the same with const - what happens?" = It shows uncaught typeError
-
-
-//Exercise 3: Data Type Detective
-console.log(typeof 42);
-console.log(typeof "Hello");
-console.log(typeof true);
-console.log(typeof undefined);
-// What do you think each one will return?
-// number, string, boolean, undefined
+score ++;
+console.log("After score ++:",score)
