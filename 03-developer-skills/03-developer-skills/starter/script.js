90c308e0dede45aadadbe3abe13003a5df2cf3e8
Len Alegre <lenalegre4@gmail.com>
2025-09-09 08:41:49 +0800

    03-01_Exercise: Code Formatting Practice

--- 03-developer-skills/03-developer-skills/starter/script.js
+++ 03-developer-skills/03-developer-skills/starter/script.js
@@ -1,3 +1,13 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// Create this with terrible formatting:
+const studentTest = {
+  firstName: 'your-name',
+  skills: ['HTML', 'CSS', 'JavaScript'],
+  experience: 'beginner',
+  goals: ['become-developer', 'build-projects'],
+};
 
+const testFunc = function (data) {
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i]);
+  }
+};
