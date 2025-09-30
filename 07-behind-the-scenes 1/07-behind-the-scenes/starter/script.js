1e756736de4a6fefe1534e2b0cf97be791eea512
Len Alegre <lenalegre4@gmail.com>
2025-09-30 21:41:16 +0800

    feat(behind-the-scenes): implement Hour 2 with this keyword and arrow functions - Demonstrate this binding rules in method calls and detached functions - Show arrow functions vs regular functions with lexical this behavior - Solve common this pitfalls using arrow functions and self = this patterns - Compare arguments object in regular functions vs rest parameters - Add practical timer examples showing callback this problems and solutions - Include comprehensive this binding examples and best practices

--- 07-behind-the-scenes 1/07-behind-the-scenes/starter/script.js
+++ 07-behind-the-scenes 1/07-behind-the-scenes/starter/script.js
@@ -1,22 +1,99 @@
 'use strict';
+// const obj = {
+//   name: 'Object',
 
-console.log(varX);
-// console.log(letX);
-// console.log(constX);
+//   regularMethod: function () {
+//     console.log('Regular:', this.name);
+//   },
 
-var varX = 1;
-let letX = 2;
-const constX = 3;
 
-console.log(addDecl(2, 3));
-console.log(addExpr(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
-}
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
 
-const addExpr = function (a, b) {
-  return a + b;
+// obj.regularMethod();
+// obj.arrowMethod();
+
+// // Before running, predict the output:
+// const quiz = {
+//   name: 'Quiz Object',
+//   regularMethod() {
+//     console.log('Regular:', this.name);
+//   },
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// quiz.regularMethod(); // Predict: ?
+// quiz.arrowMethod(); // Predict: ?
+
+// const timer = {
+//   name: 'Timer',
+//   // using the old ways
+//   start: function () {
+//     console.log(`${this.name} starting...`);
+
+//     const self = this;
+
+//     setTimeout(function () {
+//       console.log(`${self.name} finsihed`);
+//     }, 1000);
+//   },
+//   // Using arrow function
+//   startModern: function () {
+//     console.log(`${this.name} starting modern...`);
+
+//     setTimeout(() => {
+//       console.log(`${this.name} finished modern`);
+//     });
+//   },
+// };
+
+// timer.start();
+// timer.startModern();
+
+// const user = {
+//   name: 'Alice',
+//   hobbies: ['reading', 'coding', 'gaming'],
+
+//   // BROKEN: arrow function loses `this`
+//   printHobbiesBad: () => {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`); // this.name is undefined!
+//     });
+//   },
+
+//   // FIXED: regular function preserves `this`
+//   printHobbiesGood() {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`); // this.name works!
+//     });
+//   },
+// };
+
+// // user.printHobbiesBad();
+// user.printHobbiesGood();
+
+const functionTypes = {
+  regularFunction: function () {
+    console.log('Arguemtns length', arguments.length);
+    console.log(`First argument:`, arguments[0]);
+  },
+
+  arrowFunction: () => {
+    console.log(arguments);
+    console.log('Arrow function called');
+  },
+
+  modernFunction: (...args) => {
+    console.log('Args length:', args.length);
+    console.log('First arg', args[0]);
+  },
 };
 
-const addArrrow = (a, b) => a + b;
+functionTypes.regularFunction('Hello', 'world');
+// functionTypes.arrowFunction('test');
+functionTypes.modernFunction('modern', 'approach');
